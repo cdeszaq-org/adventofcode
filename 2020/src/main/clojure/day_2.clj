be96aef43010a9da01a28becb19014be0118fb48
Jensen <rickjens@amazon.com>
2020-12-02 03:12:37 -0600

    Day 2, part 2 solution.

--- 2020/src/main/clojure/day_2.clj
+++ 2020/src/main/clojure/day_2.clj
@@ -1,15 +1,26 @@
 (ns day-2
   (:require [util :refer :all]))
 
-(defn valid?
+(defn valid-count?
   "Determine if the password in the policy is valid. Valid means that the letter appears between
   min and max times in the password (inclusive)"
   [{:keys [min max letter string]}]                         ; Unpack the map to validate
   (->> string
-       concat                                               ; Make string a seq
        (filter #(= letter %))                               ; Strip non-chars
        count
        (#(<= min % max))))                                  ; Is count within bounds?
+
+(defn valid-positions?
+  "Determine if the password in the policy is valid. Valid means that exactly one letter in the
+  min and max positions (1-indexed) matches the policy's letter"
+  [{:keys [min max letter string]}]                         ; Unpack the map to validate
+  (let [first (get string (dec min))                        ; Get the letters at min and max positions (1-indexed)
+        second (get string (dec max))]
+    (and (not (= first second))                             ; If they are the same, invalid since more than 1 might match
+         (or (= first letter)                               ; See if one of them matches
+             (= second letter)))
+    )
+  )
 
 (defn- parse-min-max
   "Parse the min-max component of a policy into a min and max component"
@@ -39,13 +50,14 @@
        (map parse-policy)))                                 ; Map the rows to policy shapes
 
 (defn solve-puzzle
-  []
+  [validity]
   (->> "day_2"                                              ; Problem input
        deserialize
-       (filter valid?)
+       (filter validity)
        count))
 
 (defn -main
   "Print the solution in a human-friendly way"
   []
-  (println "Day 2, puzzle 1:" (solve-puzzle)))
+  (println "Day 2, puzzle 1:" (solve-puzzle valid-count?))
+  (println "Day 2, puzzle 1:" (solve-puzzle valid-positions?)))
--- 2020/src/test/clojure/day_2_test.clj
+++ 2020/src/test/clojure/day_2_test.clj
@@ -2,10 +2,15 @@
   (:require [clojure.test :refer :all]
             [day-2 :refer :all]))
 
-(deftest examples-validate
-  (is (true? (valid? {:min 1 :max 3 :letter \a :string "abcde"})))
-  (is (false? (valid? {:min 1 :max 3 :letter \b :string "cdefg"})))
-  (is (true? (valid? {:min 2 :max 9 :letter \c :string "ccccccccc"}))))
+(deftest examples-validate-positions
+  (is (true? (valid-positions? {:min 1 :max 3 :letter \a :string "abcde"})))
+  (is (false? (valid-positions? {:min 1 :max 3 :letter \b :string "cdefg"})))
+  (is (false? (valid-positions? {:min 2 :max 9 :letter \c :string "ccccccccc"}))))
+
+(deftest examples-validate-count
+  (is (true? (valid-count? {:min 1 :max 3 :letter \a :string "abcde"})))
+  (is (false? (valid-count? {:min 1 :max 3 :letter \b :string "cdefg"})))
+  (is (true? (valid-count? {:min 2 :max 9 :letter \c :string "ccccccccc"}))))
 
 (deftest examples-parse
   (is (= {:min 1 :max 3 :letter \a :string "abcde"} (parse-policy "1-3 a: abcde")))

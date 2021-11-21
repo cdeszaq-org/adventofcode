70824eecbc646b4645137676ca448c083b44bd5a
Jensen <rickjens@amazon.com>
2021-11-21 01:08:26 -0600

    Adjust namespace to adventofcode.

--- src/com/cdeszaq/adventofcode/twenty15.cljs
+++ src/com/cdeszaq/adventofcode/twenty15.cljs
@@ -1,4 +1,4 @@
-(ns com.cdeszaq.advent.twenty15
+(ns com.cdeszaq.adventofcode.twenty15
   (:require [cljs.spec.alpha :as s]
             [clojure.math.combinatorics :as combo]
             [clojure.string :as str]))
--- test/com/cdeszaq/adventofcode/twenty15_test.cljs
+++ test/com/cdeszaq/adventofcode/twenty15_test.cljs
@@ -1,6 +1,6 @@
-(ns com.cdeszaq.advent.twenty15-test
+(ns com.cdeszaq.adventofcode.twenty15-test
   (:require [cljs.test :refer-macros [deftest testing is are]]
-            [com.cdeszaq.advent.twenty15 :as adv]))
+            [com.cdeszaq.adventofcode.twenty15 :as adv]))
 
 (deftest day1
   (testing "part 1: "

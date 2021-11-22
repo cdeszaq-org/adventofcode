a253ccbd3000752b0389ae73c7ab940c458ff75a
Jensen <rickjens@amazon.com>
2021-11-21 23:06:01 -0600

    Day 3, part 1, and some of part 2.

---
+++ src/com/cdeszaq/adventofcode/twenty15/day3.cljs
@@ -0,0 +1,57 @@
+(ns com.cdeszaq.adventofcode.twenty15.day3
+  (:require [clojure.string :as str]))
+
+
+; Day 3, Part 1
+; ...This is the elevator problem, but in 2 dimensions... Perhaps generalize it later?
+; Convert to directions
+; Convert directions to grid deltas. This is the "path"
+; Convert directions to running grid locations (reductions). This is the "map"
+; Group by grid locations
+; Count keys
+
+(def input-to-directions {\^ ::up \> ::right \v ::down \< ::left})
+
+(def directions-to-deltas {::up [0 1] ::right [1 0] ::down [0 -1] ::left [-1 0]})
+
+(defn parse-directions
+  [input]
+  (map input-to-directions input))
+
+(defn apply-delta
+  ""
+  [current delta]
+  (map + current delta))
+
+(defn house-count
+  ""
+  [input]
+  (->> input
+       (parse-directions)
+       (map directions-to-deltas)
+       (reductions apply-delta [0 0])
+       (group-by identity)
+       (keys)
+       (count)))
+
+; Day 3, Part 2
+(defn group-by-kv
+  "Returns a map of the elements of coll keyed by the result of f on 
+   the key and value of each element. f should be a function of 2 
+   arguments. The value at each key will be a vector of the 
+   corresponding elements, in the order they appeared in coll. Note
+   that group-by-kv is supported on vectors, where the keys will be 
+   the ordinals."
+  [f coll]
+  (persistent!
+   (reduce-kv
+    (fn [ret key val]
+      (let [grouping-key (f key val)
+            old-grouping-val (get ret grouping-key [])
+            new-grouping-val (conj old-grouping-val val)]
+        (assoc! ret grouping-key new-grouping-val)))
+    (transient {}) 
+    coll)))
+
+(comment (reduce-kv (fn [acc key val] (assoc acc key (count val))) {} [])
+         (reduce))
---
+++ test/com/cdeszaq/adventofcode/twenty15/day3_test.cljs
@@ -0,0 +1,27 @@
+(ns com.cdeszaq.adventofcode.twenty15.day3-test
+  (:require [cljs.test :refer-macros [deftest testing is are]]
+            [com.cdeszaq.adventofcode.twenty15.day3 :as aoc]
+            [com.cdeszaq.adventofcode.twenty15.day3-input :as input :refer [puzzle-input]]))  
+   
+(deftest day3
+  (testing "part 1: "
+    (testing "the right number of houses are counted"
+      (are [houses directions]
+           (= houses (aoc/house-count directions))
+        2 ">"
+        4 "^>v<"
+        2 "^v^v^v^v^v")
+
+      (testing "puzzle input"
+        (is (= 2081 (aoc/house-count puzzle-input))))))
+
+  (testing "part 2: "
+    (testing "the right number of houses are counted"
+      (are [houses directions]
+           (= houses (aoc/house-count directions))
+        3 ">"
+        3 "^>v<"
+        11 "^v^v^v^v^v")
+
+      (testing "puzzle input"
+        (is (= 0 (aoc/house-count puzzle-input)))))))
